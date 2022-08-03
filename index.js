47b25f5b2b067e7824f78165336301b122b47453
Victor ADASCALITEI <210908+3Nigma@users.noreply.github.com>
2022-08-03 20:35:49 +0300

    Pretty code. Just the way we like it

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-require('dotenv').config()
 const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs-extra');
-const path = require('path');
-const {totalist} = require('totalist/sync');
+const oFs = require('fs');
+const fs = require('fs/promises');
+const fsE = require("fs-extra");
 const server = express();
 const PORT = process.env.PORT || 8080;
+const CACHE_BASE_PATH = process.env.CACHE_BASE_PATH || 'cache';
 
-server.use(bodyParser.json());
-server.use(bodyParser.raw({
+let inboundCaches = {};
+
+fsE.mkdirpSync(CACHE_BASE_PATH);
+
+server.use(express.json());
+server.use(express.raw({
     type: 'application/octet-stream',
     limit: '50mb'
-}))
+}));
 
 server.use((req, res, next) => {
     if (req.get('Authorization') !== `Bearer ${process.env.AUTH_KEY}` &&
@@ -22,111 +25,59 @@ server.use((req, res, next) => {
     }
 })
 
-server.get('/', (req, res) => {
-    res.status(200).send({
-        status: 'success'
-    })
-})
-
-server.post('/_apis/pipelines/workflows/:runId/artifacts', (req, res) => {
-    const {runId} = req.params;
-    const baseURL = `${req.protocol}://${req.get('host')}${req.baseUrl}`;
-
-    res.json({fileContainerResourceUrl: `${baseURL}/upload/${runId}`});
-});
-
-server.patch('/_apis/pipelines/workflows/:runId/artifacts', (req, res) => {
-    const {runId} = req.params;
-
-    res.status(200).json({message: 'success'});
-});
-
-server.get('/_apis/pipelines/workflows/:runId/artifacts', (req, res) => {
-    const {runId} = req.params;
-    const artifacts = new Set();
-   const baseURL = `${req.protocol}://${req.get('host')}${req.baseUrl}`;
-    totalist(`./${runId}`, (name, abs, stats) => {
-        name = name.replace('\\', '/');
-        const fileDetails = {
-            name: name.split('/')[0],
-            fileContainerResourceUrl: `${baseURL}/download/${runId}`
-        }
-        artifacts.add(fileDetails);
-    });
-    console.log(artifacts);
-    res.status(200).json({count: artifacts.count, value: [...artifacts]});
-});
-
-server.get('/download/:container', (req, res) => {
-    const {container} = req.params;
-    const baseURL = `${req.protocol}://${req.get('host')}${req.baseUrl}`;
-    const files = new Set();
-    totalist(container, (name, abs, stats) => {
-        console.log(name);
-        console.log(abs);
-        files.add({
-            path: path.normalize(name),
-            itemType: 'file',
-            contentLocation: `${baseURL}/download/${container}/${name.replace('\\', '/')}`
-        });
-    })
-    res.status(200).json({value: [...files]})
-});
-
-server.get('/download/:container/:path(*)', (req, res) => {
-    const path = `${req.params.container}/${req.params.path}`;
-    fs.createReadStream(path, {encoding: 'utf-8'}).pipe(res);
-});
-
-server.put('/upload/:runId', (req, res, next) => {
-    const { itemPath } = req.query;
-    const {runId} = req.params;
-    req.setEncoding('base64');
-    fs.ensureFileSync(`${runId}/${itemPath}`);
-    fs.writeFile(`${runId}/${path.normalize(itemPath)}`, req.body, {encoding: 'utf-8'}, (err) => {
-        if (err) {
-            console.error(err);
-        }
-        res.status(200).json({message: 'success'})
-    });
-});
-
-server.get('/_apis/artifactcache/cache', (req, res, next) => {
+// Get cache entry
+server.get('/_apis/artifactcache/cache', async (req, res, next) => {
+    // TODO: what happens if multiple keys are provided?
     const { keys, version } = req.query;
     const baseURL = `${req.protocol}://${req.get('host')}${req.baseUrl}`;
-    // TODO: what happens if multiple keys are provided?
-    const cacheLocation = `${keys}_${version}`;
-    const archiveLocation = fs.existsSync(cacheLocation) ? `${baseURL}/_apis/artifactcache/cache/download/${cacheLocation}` : "";
+    const cacheId = `${keys}_${version}`;
+    const cacheLocation = `${CACHE_BASE_PATH}/${cacheId}`;
+    let archiveLocation = "";
 
-    res.status(200).json({archiveLocation});
+    try {
+        await fs.access(cacheLocation, oFs.constants.F_OK);
+        archiveLocation = `${baseURL}/_apis/artifactcache/cache/download/${cacheId}`;
+    } catch(e) { /* No-op */ }
+    res.status(200).json({archiveLocation, cacheKey: keys});
 });
 
+// Download cache
 server.get('/_apis/artifactcache/cache/download/:cacheId', (req, res, next) => {
     const { cacheId } = req.params;
-    const file = `${__dirname}/${cacheId}`;
+    const file = `${__dirname}/${CACHE_BASE_PATH}/${cacheId}`;
     
-    res.sendFile(file);
+    res.download(file);
 });
 
-server.post('/_apis/artifactcache/caches', (req, res, next) => {
+// Reserve cache
+server.post('/_apis/artifactcache/caches', async (req, res, next) => {
     const { key, version, cacheSize } = req.body;
+    const cacheId = `${key}_${version}`;
+    const cacheLocation = `${CACHE_BASE_PATH}/${cacheId}`;
+    const fh = await fs.open(cacheLocation, 'w');
 
-    res.status(200).json({cacheId: `${key}_${version}`});
+    inboundCaches[cacheId] = { fh };
+    res.status(200).json({ cacheId });
 });
 
-server.patch('/_apis/artifactcache/caches/:cacheId', (req, res, next) => {
+// Save cache
+server.patch('/_apis/artifactcache/caches/:cacheId', async (req, res, next) => {
     const { cacheId } = req.params;
-    fs.writeFile(`${cacheId}`, req.body, {encoding: 'utf-8'}, (err) => {
-        if (err) {
-            console.error(err);
-        }
-        res.status(200).json({message: 'success'})
-    });
-    // res.status(200).json({cacheId: `${key}_${version}`});
+    const contentRange = req.headers['content-range'];
+    const [ _, startIndex, endIndex ] = /(\d+)-(\d+)/g.exec(contentRange);
+
+    await inboundCaches[cacheId].fh.write(req.body, 0, req.body.length, +startIndex);    
+    res.status(200).end();
 });
 
+// Commit cache
 server.post('/_apis/artifactcache/caches/:cacheId', (req, res, next) => {
+    const { cacheId } = req.params;
     const { size } = req.body;
+
+    // TODO: make use of provided size
+    inboundCaches[cacheId].fh.close();
+    delete inboundCaches[cacheId];
     res.status(200).end();
 });
 
